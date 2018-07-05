b24c68dedd2ad17fe296393fb96e99d7ea256f5d
DESKTOP-IG0L9JQ\lenovo <huangcf>
2018-07-05 18:04:23 +0800

    接入企业微信

--- controllers/codesquare/SiteController.php
+++ controllers/codesquare/SiteController.php
@@ -20,8 +20,14 @@ class SiteController extends CSBaseController
     public function actionIndex(){
         //echo "hello code square";
 
-        echo $_GET['code'];
+        $code =   $_GET['code'];
+        $access_token = 'Ik3kDatzL4KOLSQz0BiyjbB6t9LhnMd9QSCPLfDwUfvzTj4Fn00saB5qTSYjv8Al5KbrKEmgwQX7pooO47bZ3w';
+        $url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token={$access_token}&code={$code}";
+        $user = file_get_contents($url);
+        var_dump($user);die;
 
+       //获取用户信息
+    //    $userInfoUrl = ""
     }
 
 }
