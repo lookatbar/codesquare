931076ede6e4e6b94522b2911df694483c8a432a
unknown <zhugm@mysoft.com.cn>
2018-07-08 00:15:53 +0800

    我收到的回复

--- models/cs/ReplyModel.php
+++ models/cs/ReplyModel.php
@@ -60,18 +60,30 @@ class ReplyModel extends CSBaseModel
 
 
     public function getBeRepliedListByUser($userId,$pageIndex=CSConstant::PAGE_INDEX,$pageSize=CSConstant::PAGE_SIZE){
-        return (new \yii\db\Query())
-            ->from('cs_reply')
-            ->where(['to_user_id' => $userId, 'is_deleted' => 0])
-            ->limit($pageSize)
-            ->offset($pageIndex)
-            ->all($this->db);
+//        return (new \yii\db\Query())
+//            ->from('cs_reply')
+//            ->where(['to_user_id' => $userId, 'is_deleted' => 0])
+//            ->limit($pageSize)
+//            ->offset($pageIndex)
+//            ->all($this->db);
 
-        $fields = static::$Fields;
-        $countSql = 'SELECT COUNT(1) FROM cs_topic WHERE 1=1 AND is_deleted=0';
-        $limitSql = "SELECT $fields  FROM cs_topic LEFT JOIN cs_user ON cs_topic.user_id=cs_user.user_id WHERE 1=1 AND is_deleted=0 ".$this->_getLimitSql([$pageIndex,$pageSize]);
+        if(empty($userId)){
+            return $this->retPage([],0);
+        }
 
-        $where = " offer_award_id>0 AND reward_money>0 ";
+        $fields = ' cs_reply.reply_id
+        ,cs_reply.topic_id
+        ,cs_reply.content
+        ,cs_reply.image_list
+        ,cs_reply.create_time
+        ,cs_topic.title
+        ,cs_topic.topic_type
+        ,cs_user.name as user_name
+        ,cs_user.avatar as user_avatar ';
+        $countSql = 'select COUNT(1) from cs_reply LEFT JOIN cs_topic ON cs_reply.topic_id=cs_topic.topic_id WHERE 1=1';
+        $limitSql = "select $fields from cs_reply LEFT JOIN cs_topic ON cs_reply.topic_id=cs_topic.topic_id  LEFT JOIN cs_user ON cs_reply.user_id=cs_user.user_id WHERE 1=1 ".$this->_getLimitSql([$pageIndex,$pageSize]);
+
+        $where = " cs_topic.user_id=$userId AND cs_reply.is_deleted=0 AND cs_topic.is_deleted=0 ";
         $countSql = str_replace('1=1',$where,$countSql);
         $limitSql = str_replace('1=1',$where,$limitSql);
 
