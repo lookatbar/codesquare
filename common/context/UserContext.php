92db2acd325f5d2cb7c4ffbb3897f3e55ed21a59
chenxy <chenxy@mysoft.com.cn>
2018-07-07 13:55:07 +0800

    控制器基类

---
+++ common/context/UserContext.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\common\context;
+
+/**
+ * 用户上下文
+ *
+ * @author ray-apple
+ */
+class UserContext
+{
+     /**
+     * 当前用户id
+     * @var string
+     */
+    public $userId;
+    
+    /**
+     * 当前用户名称
+     * @var string
+     */
+    public $userName;
+}
--- controllers/codesquare/CSBaseController.php
+++ controllers/codesquare/CSBaseController.php
@@ -9,7 +9,7 @@
 namespace app\controllers\codesquare;
 
 use app\controllers\BaseController;
-
+use app\common\context\UserContext;
 
 /**
  * 代码广场基础类
@@ -18,12 +18,47 @@ use app\controllers\BaseController;
  */
 class CSBaseController extends BaseController
 {
-
-    protected $userInfo = NULL;
-
-    public function init()
+    /**
+     * 会话超时时间,单位秒
+     */
+    const SESSION_TIME_OUT = 7200;
+        
+    /**
+     * 当前用户上下文
+     * @var \app\common\context\UserContext
+     */
+    protected $userContext = null;
+    
+    public function beforeAction($action)
     {
-        //校验并且初始化用户数据
-
+        if (!parent::beforeAction($action)) {
+           return false;
+        }
+        
+        // 检查token
+        $cache = \Yii::$app->cache;
+        $token = $this->getToken();
+        if (!$token || !$cache->exists($token)) {
+            \Yii::$app->response->data = $this->error('token无效', '403', $token);
+            \Yii::$app->end();
+        }
+        
+        // 初始化用户上下文对象并刷新token有效期
+        $userContext = $cache->get($token);
+        if ($userContext !== false) {
+           $this->userContext = $userContext;
+           $cache->set($token, $userContext, static::SESSION_TIME_OUT);
+        }
+        
+        return true;
+    }
+    
+    /**
+     * 获取token值
+     * @return string
+     */
+    protected function getToken()
+    {
+        return \Yii::$app->request->post('token');
     }
 }
