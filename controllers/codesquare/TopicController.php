0a05682cd394d997406855bbfa97ea061f556fa4
unknown <zhugm@mysoft.com.cn>
2018-07-07 09:46:24 +0800

    定义各个接口

---
+++ controllers/codesquare/TopicController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * 主题控制类
+ */
+
+namespace app\controllers\codesquare;
+
+
+class TopicController extends CSBaseController
+{
+
+
+    /**
+     * 广场首页
+     */
+    public function actionIndex(){
+            echo  "ooooooo ? userId = ";
+    }
+
+
+    /**
+     * 查看详情页面
+     */
+    public function actionTopicDetail(){
+
+    }
+
+
+    /**
+     * 保存
+     */
+    public function actionTopicSave(){
+
+    }
+
+    /**
+     * 标签列表
+     */
+    public function actionTagList(){
+
+    }
+
+
+    /**
+     * 回复
+     */
+    public function actionReply(){
+
+    }
+
+
+    /**
+     * 删除回复
+     */
+    public function actionDelReply(){
+
+    }
+
+    /**
+     * 点赞/取消点赞
+     */
+    public function  actionLike(){
+
+    }
+
+    /**
+     * 打赏
+     */
+    public function actionReward(){
+
+    }
+
+
+
+
+
+
+}
---
+++ controllers/codesquare/UserController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * 用户模块控制器
+ */
+
+namespace app\controllers\codesquare;
+
+
+class UserController extends CSBaseController
+{
+
+    /**
+     * 用户个人中心
+     */
+    public function actionIndex(){
+
+    }
+
+
+    public function actionMsgList(){
+
+    }
+
+    /**
+     * 收到恢复
+     */
+    public function actionBeRepliedList(){
+
+    }
+
+    /**
+     * 收到点赞
+     */
+    public function actionBeLikedList(){
+
+    }
+
+    /**
+     * 收到的打赏
+     */
+    public function actionBeRewardList(){
+
+    }
+
+    /**
+     * 收支明细
+     */
+    public function actionPaymentsList(){
+
+    }
+
+    /**
+     * 发送的话题列表
+     */
+    public function actionTopicList(){
+
+    }
+
+
+}
