ba6d1998e7c6c70166b54f905ba96721bdc678b1
杨泳樟 <yangyz@mingyuanyun.com>
2018-07-07 20:56:19 +0800

    修改请求根路径

--- web/rc.conf.js
+++ web/rc.conf.js
@@ -1,7 +1,8 @@
 module.exports = {
 	proxy: {
-		'/wx': 'http://jkds.cracher.top',
-		'/api': 'http://10.5.23.119:8030',
+		// '/wx': 'http://jkds.cracher.top',
+		// '/api': 'http://10.5.23.119:8030',
+		'/api': 'http://jkds.cracher.top',
 	},
 	// assetPath: 'static',
 }
